c5c80f2769836cd290dd7b7ba129efd6bf482b86
Abzol <hakurou46@gmail.com>
2018-07-21 20:48:15 +0200

    Updated for modern sheet

--- caitbot.py
+++ caitbot.py
@@ -2,6 +2,7 @@
 
 import requests
 import re
+import sys
 
 transdict = {"sword" : "Swords", "spear" : "Polearms", "ax" : "Axes",
              "dagger" : "Daggers", "bow" : "Bows", "rod" : "Staves"}
@@ -12,22 +13,29 @@ def maketrans(transdict):
     return trans
 
 def csv2dict(name):
-    head = ["name", "region", "shield", "hp", "weak", "gold",
+    head = ["name", "shield", "hp", "weak", "gold",
             "exp", "jp", "item"]
     data = {}
-    with open("bestiary.csv", "r") as csv:
-        for line in csv:
-            if line.startswith(name):
-                linesplit = line.split(',')
-                info = linesplit[:4]
-                info.append(linesplit[4:9])
-                info.extend(linesplit[9:])
-                data = dict(zip(head,info))
-                weaks = " ".join(data["weak"])
-                weaks = re.sub("\w+", maketrans(transdict), weaks)
-                weaks = re.sub(" N/A", "", weaks)
-                data["weak"] = weaks.split()
-                return data
+    try:
+        with open("bestiary.csv", "r") as csv:
+            for line in csv:
+                if line.startswith(name):
+                    linesplit = line.split(',')
+                    info = linesplit[:3]
+                    info.append(linesplit[3:8])
+                    info.extend(linesplit[8:])
+                    data = dict(zip(head,info))
+                    weaks = " ".join(data["weak"])
+                    weaks = re.sub("\w+", maketrans(transdict), weaks)
+                    weaks = re.sub(" N/A", "", weaks)
+                    data["weak"] = weaks.split()
+                    return data
+    except FileNotFoundError:
+        print("Bestiary not found!")
+        print("Did you download it?")
+        print(" -> clone-sheet")
+        sys.exit()
+
 def diff(a, b):
     miss = False
     for key, value in a.items():
@@ -38,28 +46,30 @@ def diff(a, b):
     if miss == False:
         print("No misses!")
 
-with open("Cait", "r") as wiki:
-    data = {}
-    PARSING = False
-    for line in wiki:
-        if "{{Template:Enemy" in line:
-            PARSING = True
-        if PARSING:
-            if line.startswith("}}"):
-                PARSING = False
-                continue
-            scan = re.findall("\|(.*?)[ =]+(.*?)[\n\|}]", line)
-            for item in scan:
-                data[item[0]] = item[1]
-            if "}}" in line:
-                PARSING = False
-                continue
-    # strip formatting
-    for key, value in data.items():
-        scan = re.findall("(\w*?)\.png", value)
-        if not scan == []:
-            data[key] = scan
+if __name__ == "__main__":
+    with open("Cait", "r") as wiki:
+        data = {}
+        PARSING = False
+        for line in wiki:
+            if any(x in line for x in ["{{Template:Enemy",
+                                       "{{Template:Character"]):
+                PARSING = True
+            if PARSING:
+                if line.startswith("}}"):
+                    PARSING = False
+                    continue
+                scan = re.findall("\|(.*?)[ =]+(.*?)[\n\|}]", line)
+                for item in scan:
+                    data[item[0]] = item[1]
+                if "}}" in line:
+                    PARSING = False
+                    continue
+        # strip formatting
+        for key, value in data.items():
+            scan = re.findall("(\w*?)\.png", value)
+            if not scan == []:
+                data[key] = scan
 
-    a = data
-    b = csv2dict("Cait")
-    diff (a, b)
+        a = data
+        b = csv2dict("Cait")
+        diff (a, b)
